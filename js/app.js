463f172e17090b9d1764c43881bb5e0920d89c85
colettekenney <colettemkenney@gmail.com>
2016-03-22 21:22:57 -0600

    Add reset button

--- js/app.js
+++ js/app.js
@@ -1,26 +1,29 @@
 $(document).ready(function() {
-	$("button").click(function() {
-		for (i=1; i<31; i++) 
+	$("button#play").click(function() {
+
+		for (i=1; i<31; i++) {
 			if((i%3) + (i%5) === 0){
-			$("body").append("FizzBuzz"+"<br/>");
+				$("#results").append("FizzBuzz"+"<br/>");
 			}
 			
 			else if (i%3 === 0){
-			$("body").append("Fizz"+"<br/>");
+				$("#results").append("Fizz"+"<br/>");
 			}
 
 			else if (i%5 === 0){
-			$("body").append("Buzz"+"<br/>");
+				$("#results").append("Buzz"+"<br/>");
 			}
 
 			else {
-			$("body").append(i+"<br/>");
+				$("#results").append(i+"<br/>");
+			}
 		}
 				
-	})
+	});
 
-
-
+	$("button#reset").click(function(){
+		$("#results").html("");
+	});
 
 
 })
