66d3bccf9b9e8f5d460fb47120d4d515a52d7264
colettekenney <colettemkenney@gmail.com>
2016-03-24 11:02:33 -0600

    refactor by adding function

--- js/app.js
+++ js/app.js
@@ -3,24 +3,11 @@ $(document).ready(function() {
 	$("button#play").click(function() {
 
 		var userChoice = +(document.getElementById('user-choice').value);
-		
-
-		for (i=1; i<(userChoice +1); i++) {
-			if((i%3) + (i%5) === 0){
-				$("#results").append("FizzBuzz"+"<br/>");
-			}
-			
-			else if (i%3 === 0){
-				$("#results").append("Fizz"+"<br/>");
-			}
-
-			else if (i%5 === 0){
-				$("#results").append("Buzz"+"<br/>");
-			}
-
-			else {
-				$("#results").append(i+"<br/>");
-			}
+		if (userChoice < 1 || userChoice > 100) {
+			alert("Please Enter a number between 1 and 100");
+		} 
+		else {
+			checkNumber(userChoice);
 		}
 				
 	});
@@ -29,5 +16,23 @@ $(document).ready(function() {
 		$("#results").html("");
 	});
 
+	function checkNumber (userChoice){
+		for (i=1; i<(userChoice +1); i++) {
+				if((i%3) + (i%5) === 0){
+					$("#results").append("FizzBuzz"+"<br/>");
+				}
+				
+				else if (i%3 === 0){
+					$("#results").append("Fizz"+"<br/>");
+				}
 
+				else if (i%5 === 0){
+					$("#results").append("Buzz"+"<br/>");
+				}
+
+				else {
+					$("#results").append(i+"<br/>");
+				}
+			}
+	}
 })
