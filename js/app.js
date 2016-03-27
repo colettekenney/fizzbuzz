769fe700bcee933672390c9c9f3b1e0553918731
colettekenney <colettemkenney@gmail.com>
2016-03-27 09:12:51 -0600

    clean up javascript for final submit

--- js/app.js
+++ js/app.js
@@ -1,38 +1,31 @@
 $(document).ready(function() {
 	
-	$("button#play").click(function() {
-
-		var userChoice = +(document.getElementById('user-choice').value);
-		if (userChoice < 1 || userChoice > 100) {
-			alert("Please Enter a number between 1 and 100");
-		} 
-		else {
-			checkNumber(userChoice);
-		}
-				
+	$('form').submit(function(e) {
+		e.preventDefault();
+		var userChoice = +($('#user-choice').val());
+		//console.log(userChoice);
+		playFizzBuzz(userChoice);
 	});
 
-	$("button#reset").click(function(){
+	$("#reset").click(function(){
 		$("#results").html("");
+		$("#user-choice").val("1");
 	});
 
-	function checkNumber (userChoice){
+	function playFizzBuzz(userChoice){
 		for (i=1; i<(userChoice +1); i++) {
-				if((i%3) + (i%5) === 0){
-					$("#results").append("FizzBuzz"+"<br/>");
-				}
-				
-				else if (i%3 === 0){
-					$("#results").append("Fizz"+"<br/>");
-				}
-
-				else if (i%5 === 0){
-					$("#results").append("Buzz"+"<br/>");
-				}
-
-				else {
-					$("#results").append(i+"<br/>");
-				}
+			if((i%3) + (i%5) === 0){
+				$("#results").append("FizzBuzz"+"<br/>");
 			}
+			else if (i%3 === 0){
+				$("#results").append("Fizz"+"<br/>");
+			}
+			else if (i%5 === 0){
+				$("#results").append("Buzz"+"<br/>");
+			}
+			else {
+				$("#results").append(i+"<br/>");
+			}
+		}
 	}
-})
+});
